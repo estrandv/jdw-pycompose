a0efe6c732390638b7a7c413ba50f5d9be5b14fc
Emstrevk <emil.strandvik@gmail.com>
2022-07-30 20:36:11 +0300

    Working test for all parses

--- osc_transform.py
+++ osc_transform.py
@@ -5,6 +5,7 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 import new_parsing_july
 import scales # TODO: This needs to come along
+import time
 
 def to_timed_osc(time: float, msg):
     bundle = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
@@ -23,10 +24,24 @@ class MessageWrapper:
     def __init__(self, message: new_parsing_july.Message):
         self.message = message
 
+    def to_sample_play(self):
+        #time = self.message.args["sus"] if "sus" in self.message.args else 0.0
+        # Silly default - not sure what a good other option is if tone is not mandatory 
+        ext_id = self.message.suffix if self.message.suffix != "" else "sample" + ",".join(self.message.args)
+        osc_args = []
+        for key in self.message.args:
+            osc_args.append(key)
+            osc_args.append(self.message.args[key])
+        return create_msg("/play_sample", [ext_id, "example", self.message.index, self.message.prefix] + osc_args)
+
     def to_note_on_timed(self, synth: str):
 
+        # TODO: Hack for now - symbol resolution is better
+        if synth == "sample":
+            return self.to_sample_play()
+
         if "freq" not in self.message.args:
-            self.message.create_freq_arg(scales.MAJOR, 3)
+            self.message.create_freq_arg(scales.MAJOR, 2)
 
         time = self.message.args["sus"] if "sus" in self.message.args else 0.0
         # Silly default - not sure what a good other option is if tone is not mandatory 
@@ -42,25 +57,40 @@ class MessageWrapper:
     
 # TODO: Just gonna play with it here for now 
 
+# "((0/(20/28)/22/(19/25))[attT0 >0.1 relT2 =0 #0.2 lfoS0.01 lfoD0.1] 1 3 (1 5)[=0.25] 2)[=0.5 #1.0 relT0.5 attT0.05 >0.1]"
+
 # Hardcoded default port of jdw-sequencer main application
-client = udp_client.SimpleUDPClient("127.0.0.1", 14441)
 
-main_bundle = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
-main_bundle.add_content(create_msg("/bundle_info", ["update_queue"]))
-main_bundle.add_content(create_msg("/update_queue_info", ["python_test_queue"]))
 
-bun = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
-# TODO: Somehow there is a get_bundle(1) error for the full queue message if we have too many
-# notes in the parse..? 
-# UPDATE: Might actually be something with the length of the parse....? 
-for msg in new_parsing_july.full_parse("(0[>4 lfoS22.2 lfoD0.2] (9/8)[fx3.0 >1.0 #0.5] 4)[=1.0 #1.0 relT0.0 attT0.05 >0.1]"):
-    noti = MessageWrapper(msg)
-    time = noti.get_time()
-    wrp = to_timed_osc(noti.get_time(), noti.to_note_on_timed("brute"))
-    print("Msg info ", msg.__dict__)
+class OSCSender:
+    def __init__(self):
 
-    bun.add_content(wrp)
+        self.client = udp_client.SimpleUDPClient("127.0.0.1", 14441)
 
-main_bundle.add_content(bun.build())
+    def send(self, parse_string: str, synth = "gentle"):
 
-client.send(main_bundle.build())
+        main_bundle = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
+        main_bundle.add_content(create_msg("/bundle_info", ["update_queue"]))
+        main_bundle.add_content(create_msg("/update_queue_info", [synth + "_queue_id"]))
+
+        bun = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
+
+        messages = new_parsing_july.full_parse(parse_string)
+
+        # TODO: Get rid of all the bug investigation from earlier - it's a server side issue
+        for msg in messages:
+            noti = MessageWrapper(msg)
+            send_msg = noti.to_note_on_timed(synth)
+            #wrp = to_timed_osc(noti.get_time(), create_msg("/s_new", []))
+
+            wrp = to_timed_osc(noti.get_time(), send_msg)
+
+            bun.add_content(wrp)
+
+        main_bundle.add_content(bun.build())
+        self.client.send(main_bundle.build())
+
+sender = OSCSender()
+sender.send("(bd0[=0 ofs0] (bd1/bd1/bd1/bd1) bd2[ofs0.2] ((bd1 bd2)[=0.5]/bd1) (sn0/to1)[=0 #0.5] bd2)[>1 =1 #1]", "sample")
+sender.send("((0/(20/28)/22/(19/25))[attT0 >0.1 relT2 =0 #0.2 lfoS0.01 lfoD0.1] (1/1/1/1)[=0 #0] 1 3 (1 5)[=0.25] 2)[=0.5 #1.0 relT0.5 attT0.05 >0.1]")
+# (0/(20/28)/22/(19/25))[attT0 >0.1 relT2 =0 #0.2 lfoS0.01 lfoD0.1] 
