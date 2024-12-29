4268cc37e447be867d60a83fecd21cf6ed7a1e98
estrandv <>
2024-12-29 18:59:02 +0100

    Added confirmation beeps

--- run.py
+++ run.py
@@ -4,10 +4,24 @@ import sys
 from jdw_billboarding.lib import jdw_osc_utils
 
 BBD_ROOT = "/home/estrandv/programming/jdw-pycompose/songs/"
-BBD_FILE = "windyCity2.bbd"
+BBD_FILE = "rattlesnake.bbd"
+
+def beep():
+    song.default_client().send(jdw_osc_utils.create_msg("/note_on_timed", [
+            "blip", # SynthDef to use, see load call above
+            "confirmation_beep", # Arbitrary unique external id for the ringing note
+            "0.25", # Gate time ("note off after n sec")
+            0, # Delay execution by time
+            "freq", # Named args continue from here
+            275.0,
+            "relT",
+            0.2
+        ]))
 
 if "--update" in sys.argv:
     song.configure(BBD_ROOT + BBD_FILE)
+    beep()
+
 elif "--stop" in sys.argv:
     song.default_client().send(jdw_osc_utils.create_msg("/hard_stop", []))
     # Note that this kills any existing drones, which will have to be manually recreated
@@ -19,6 +33,7 @@ elif "--stop" in sys.argv:
     ])
     # TODO: Make setup separate
     song.setup(BBD_ROOT + BBD_FILE)
+    beep()
 
 elif "--nrt" in sys.argv:
     song.nrt_record(BBD_ROOT + BBD_FILE)
