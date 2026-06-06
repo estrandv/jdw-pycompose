1025ace91fcf7e3f732551e66cbd9a16f479f76c
estrandv <emil.strandvik@gmail.com>
2026-06-06 10:44:29 +0200

    Fix arg parsing in run.py; NRT task now passes {ZED_FILE}

--- run.py
+++ run.py
@@ -5,26 +5,20 @@ import uuid
 from jdw_billboarding.lib import jdw_osc_utils
 
 BBD_ROOT = "/home/estrandv/programming/jdw-pycompose/songs/"
-#BBD_FILE = "rattlesnake.bbd"
-#BBD_FILE = "courtRide.bbd"
-#BBD_FILE = "arena.bbd"
-#BBD_FILE = "been.bbd"
-#BBD_FILE = "windyCity2.bbd"
-#BBD_FILE = "larp.bbd"
-#BBD_FILE = "trumpets.bbd"
-BBD_FILE = "tmp.bbd"
-#BBD_FILE = "lab.bbd"
-#BBD_FILE = "vidya.bbd"
-#BBD_FILE = "lighthouse.bbd"
-#BBD_FILE = "gong.bbd"
 
-def beep(amp = 1.0):
+def resolve_bbd_path() -> str | None:
+    for arg in sys.argv[1:]:
+        if arg.endswith(".bbd"):
+            return arg
+    return None
+
+def beep(amp=1.0):
     song.default_client().send(jdw_osc_utils.create_msg("/note_on_timed", [
-            "blip", # SynthDef to use, see load call above
-            "beep-" + str(uuid.uuid4()), # Arbitrary unique external id for the ringing note
-            "0.25", # Gate time ("note off after n sec")
-            0, # Delay execution by time
-            "freq", # Named args continue from here
+            "blip",
+            "beep-" + str(uuid.uuid4()),
+            "0.25",
+            0,
+            "freq",
             875.0,
             "relT",
             0.2,
@@ -32,26 +26,27 @@ def beep(amp = 1.0):
             amp
         ]))
 
-if "--update" in sys.argv:
-    if ".bbd" in sys.argv[1]:
-        song.configure(sys.argv[1])
-    else:
-        song.configure(BBD_ROOT + BBD_FILE)
+bbd_path = resolve_bbd_path()
 
-    beep(0.1)
-
-elif "--stop" in sys.argv:
-    song.quiet(BBD_ROOT + BBD_FILE)
+if "--stop" in sys.argv:
+    song.quiet(bbd_path or BBD_ROOT + "tmp.bbd")
 
 elif "--setup" in sys.argv:
-    song.setup(BBD_ROOT + BBD_FILE)
-    song.configure(BBD_ROOT + BBD_FILE)
+    path = bbd_path or BBD_ROOT + "tmp.bbd"
+    song.setup(path)
+    song.configure(path)
     beep()
 
+elif "--update" in sys.argv:
+    if bbd_path:
+        song.configure(bbd_path)
+        beep(0.1)
+
 elif "--nrt" in sys.argv:
-    song.nrt_record(BBD_ROOT + BBD_FILE)
+    song.nrt_record(bbd_path or BBD_ROOT + "tmp.bbd")
 else:
-    if ".bbd" in sys.argv[1]:
-        song.update_queue(sys.argv[1])
-    else:
-        song.update_queue(BBD_ROOT + BBD_FILE)
+    if bbd_path:
+        song.update_queue(bbd_path)
+
+if not bbd_path and not any(f in sys.argv for f in ("--stop", "--setup", "--nrt")):
+    print(f"Usage: {sys.argv[0]} <song.bbd> [--update|--setup|--stop|--nrt]", file=sys.stderr)
